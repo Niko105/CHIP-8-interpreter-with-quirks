31e6d39168dd889bc37ec313ab3e1d1be2076120
Skye105 <umuyutregf@gmail.com>
2026-07-21 16:36:44 +0200

    fixed timings to 1000Hz for main and 60Hz for render/timers

--- src/chip8/core.clj
+++ src/chip8/core.clj
@@ -3,18 +3,12 @@
             [chip8.render.render :as render])
   (:import [java.nio.file Files Paths]))
 
-(defn- ms-from-frequency
-  "Helper function to calculate the period in milliseconds given a frequency. Truncated to int."
-  [frequency]
-  (int (* (/ 1 frequency) 1000)))
-
 (def main-frequency 1000) ;main clock frequency (CHIP8 1000Hz, SCHIP ~5000Hz)
-(def time-per-tick (ms-from-frequency main-frequency)) ;time taken on each tick (roughly)
+(def time-per-tick (int (* (/ 1 main-frequency) 1000))) ;time taken on each tick (roughly)
 (def seconds-to-run 60) ;how many seconds the sim should run for
 (def runtime (* main-frequency seconds-to-run)) ;how many ticks to run the sim for (108660000 gives rule 090 on 1dcell.ch8)
 (def fps 60) ;the fps the render thread runs at
-(def time-per-frame (ms-from-frequency fps)) ;time taken on each frame (roughly)
-(def instructions-per-frame 500) ;how many instructions to run every frame
+(def ticks-per-frame (int (/ main-frequency fps))) ;time taken on each frame (roughly)
 (def rom-path "programs/knumberknower.ch8") ;rom to load and its path
 
 (defn read-rom-into-memory
@@ -49,5 +43,6 @@
   (dotimes [i runtime]
     (cpu/tick! state)
     #_(println @state)
-    (when (= 0 (mod i instructions-per-frame)) (render/render-ascii-chip8 (:screen @state)) (Thread/sleep time-per-frame)) ;not in sync yet, timings aren't decoupled
+    (when (zero? (mod i ticks-per-frame))
+      (render/render-ascii-chip8 (:screen @state))) ;runs at ~(/ main-frequency fps), 60Hz by default
     (Thread/sleep time-per-tick)))
